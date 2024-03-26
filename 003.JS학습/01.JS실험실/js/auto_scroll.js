4a43529992ef2cd0dc66142c8a8c072251d265fd
doyeun93 <doyeun93@naver.com>
2024-03-26 13:16:20 +0900

    Update auto_scroll.js

--- 003.JS학습/01.JS실험실/js/auto_scroll.js
+++ 003.JS학습/01.JS실험실/js/auto_scroll.js
@@ -52,6 +52,14 @@ window.addEventListener('wheel',wheelFn,{passive:false});
     -> 변경해서 사용해야함 : 요소.addEventListener(이벤트명, 함수, {passive: false})
 */
 
+// [새로 고침시 스크롤바 위치 인덱싱이 되므로 맨 위로 강제 이동하기 설정]
+// scrollTo(x축이동,y축이동)
+setTimeout(() => {
+    window.scrollTo(0,0);
+}, 500);
+
+
+//////////////// 3. 함수 구현하기 ///////////////////////
 
 /*************************************** 
     함수명: wheelFn
@@ -61,32 +69,91 @@ function wheelFn(e){ // 이벤트전달변수(자동)
     // 함수호출확인!
     console.log('휠~~~!');
 
-    //// 광 휠 금지 장치
+    // 1. 우리는 휠 기본 기능을 막고 자동으로 스크롤을 하나씩 되게 할 것
+    
+    e.preventDefault();
+    // -> passive:false 설정해야함 (윈도우라서)
+    
+    //// 2. 광 휠 금지 장치
     if(stsWheel) return; // 돌아가
     stsWheel = true;  // 잠금
     setTimeout(() => {
         stsWheel = false; // 잠금해제
     }, 500);
-    // 우리는 휠 기본 기능을 막고 자동으로 스크롤을 하나씩 되게 할 것
 
-    e.preventDefault();
-    // -> passive:false 설정해야함 (윈도우라서)
 
-    // 1. 휠 방향 알아내기
+    // 3. 휠 방향 알아내기
     let delta = e.wheelDelta;
     // 휠 델타는 이벤트 객체에서 리턴해주는 방향, 이동거리 등의 정보값 
     console.log('델타값:',delta);
     // -> 마이너스가 아랫방향
 
-    // 2. 방향별 분기하기
+    // 4. 방향별 분기하기
     if(delta<0){
         // 아래페이지로 가야하니까 페이지번호 증가
         pgNum++;
+        // 한계수 체크(끝번호 고정)
+        if(pgNum === totalCnt) { pgNum = totalCnt -1;
+            // 마지막 페이지 순번은 전체개수 - 1
+        }
     } // if////
     else{
         // 반대는 윗방향이니까 페이지번호 감소
         pgNum--;
+        // 한계수 체크(0보다 작으면 고정)
+        if(pgNum < 0){
+            pgNum = 0;
+        }
     } // else //////
     console.log('pgNum: ', pgNum);
+
+
+    // 5. 페이지 이동하기
+    // 5-1. 이동할 위치알아내기 -> .page 요소중 해당 순번페이지 위치
+    let pos = elePage[pgNum].offsetTop;
+    // offsetTop : 최상단에서부터 거리
+    console.log('이동할 위치: ',pos);
+    // 5-2. 페이지 스크롤 위치 이동하기
+    // scrollTo(0, y축 이동값)
+    window.scrollTo(0, pos); 
+
+    // 6. 전체 메뉴에 on빼기
+    for(let x of gnb){
+        x.parentElement.classList.remove('on');
+    }
+    // 7. 해당 순번에 on 넣기
+    gnb[pgNum].parentElement.classList.add('on');
+    // parentElement는 선택요소의 부모요소다
+    // gnb[pgNum]은 해당순번의 메뉴 a요소다
+
 } /////////// wheelFn 함수 ////////////////
 ///////////////////////////////////////////
+
+
+
+/******************************************************* 
+    메뉴 클릭시 이벤트 처리하기 
+*******************************************************/
+// 이벤트 대상 : .gnb a
+
+const gnb = document.querySelectorAll(".gnb a");
+console.log('gnb:', gnb);
+
+// 이벤트 설정하기 + 기능 구현하기
+ gnb.forEach((ele,idx) =>{
+    ele.onclick = () =>{
+        // 클릭시 자신의 순번
+        console.log('순번 :', idx);
+        // 1. 전역페이지변수에 순번 업데이트
+        pgNum = idx;
+        // 2. 전체 메뉴에 on빼기
+        for(let x of gnb){
+            x.parentElement.classList.remove('on');
+        }
+        // 3. 해당 순번에 on 넣기
+        ele.parentElement.classList.add('on');
+        // parentElement는 선택요소의 부모요소다
+    }; /// click 함수
+
+}); /////// foreach ////////
+
