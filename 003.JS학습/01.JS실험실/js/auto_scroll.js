7276a45af99db74c81afdc9199ddb86e87f3dae8
doyeun93 <doyeun93@naver.com>
2024-03-27 15:08:48 +0900

    Update auto_scroll.js

--- 003.JS학습/01.JS실험실/js/auto_scroll.js
+++ 003.JS학습/01.JS실험실/js/auto_scroll.js
@@ -117,14 +117,17 @@ function wheelFn(e){ // 이벤트전달변수(자동)
     // scrollTo(0, y축 이동값)
     window.scrollTo(0, pos); 
 
-    // 6. 전체 메뉴에 on빼기
-    for(let x of gnb){
+    // 6. 해당 메뉴 순번 on 넣기 , 나머지 on빼기
+    chgMenu(pgNum);
+   
+   
+   /*  for(let x of gnb){
         x.parentElement.classList.remove('on');
     }
     // 7. 해당 순번에 on 넣기
     gnb[pgNum].parentElement.classList.add('on');
     // parentElement는 선택요소의 부모요소다
-    // gnb[pgNum]은 해당순번의 메뉴 a요소다
+    // gnb[pgNum]은 해당순번의 메뉴 a요소다 */
 
 } /////////// wheelFn 함수 ////////////////
 ///////////////////////////////////////////
@@ -137,23 +140,58 @@ function wheelFn(e){ // 이벤트전달변수(자동)
 // 이벤트 대상 : .gnb a
 
 const gnb = document.querySelectorAll(".gnb a");
+// 이벤트 대상 : .indic a
+
+const indic = document.querySelectorAll(".indic a");
 console.log('gnb:', gnb);
 
 // 이벤트 설정하기 + 기능 구현하기
  gnb.forEach((ele,idx) =>{
     ele.onclick = () =>{
-        // 클릭시 자신의 순번
-        console.log('순번 :', idx);
-        // 1. 전역페이지변수에 순번 업데이트
-        pgNum = idx;
-        // 2. 전체 메뉴에 on빼기
-        for(let x of gnb){
-            x.parentElement.classList.remove('on');
-        }
-        // 3. 해당 순번에 on 넣기
-        ele.parentElement.classList.add('on');
-        // parentElement는 선택요소의 부모요소다
+        // 메뉴 변경함수 호출
+        chgMenu(idx);
     }; /// click 함수
 
 }); /////// foreach ////////
+
+
+
+indic.forEach((ele,idx)=>{
+    ele.onclick = () =>{
+        // 메뉴 변경함수 호출
+        chgMenu(idx);
+    }; /// click 함수
+
+}); /////// foreach ////////
+
+
+
+
+//// [메뉴 변경함수 : .gnb + .indic] ///////////
+function chgMenu(idx){ // idx - 순번
+ 
+ console.log('순번 :', idx);
+        // 1. 전역페이지변수에 순번 업데이트
+        pgNum = idx;
+        // 2. 전체 메뉴에 on빼기
+        gnb.forEach((ele,seq)=>{
+            // ele - a요소 , seq - 순번
+            if(idx === seq){
+                // 선택순번과 같으면 on 넣기
+                ele.parentElement.classList.add('on');
+                indic[seq].parentElement.classList.add('on');
+                
+                // parentElement는 선택요소의 부모요소다        
+
+            } ///////////// if //////////
+            else{ ///// 기타의 경우 on 지우기
+                ele.parentElement.classList.remove('on');
+                indic[seq].parentElement.classList.remove('on');
+
+            }/////////////else //////////////
+    
+        });  //// foreach //////////////////
+
+        // for(let x of gnb){x.parentElement.classList.remove('on');}
+} /// chgMenu 함수 ///////////
 
