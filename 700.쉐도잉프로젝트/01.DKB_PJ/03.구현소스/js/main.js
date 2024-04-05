55a658c41e60c1db2f5fd9a4b2245176b15678f6
doyeun93 <doyeun93@naver.com>
2024-04-05 15:17:49 +0900

    s

--- 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/main.js
+++ 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/main.js
@@ -1,11 +1,19 @@
 //// 도깨비 pj 메인 js - main.js ////
 
+//  부드러운 스크롤 불러오기
+import { startSS,setScrollPos } from "./smoothScroll23.js";
 
 // 모듈로 호출된 js에서는 다른 외부 js를 import로 호출가능
 // import하려는 파일에서 반드시 함수, 변수 등을 export 해야함
 
 import slideFn from "./slide.js";
 
+/******************************************************
+ ******************** 구현 코드 파트 *******************
+ ******************************************************/
+
+// 1. 부드러운 스크롤 호출
+startSS();
 
 console.log('모듈로 메인 js 호출', document.querySelector('.top-menu'));
 
