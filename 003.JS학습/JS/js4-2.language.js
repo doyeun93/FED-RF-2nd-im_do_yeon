8c6cb52f04ff386ff8984a21af8ab02dfab56215
doyeun93 <doyeun93@naver.com>
2024-04-09 11:22:35 +0900

    s

--- 003.JS학습/JS/js4-2.language.js
+++ 003.JS학습/JS/js4-2.language.js
@@ -4,3 +4,54 @@
 import mFn from './my_function.js';
 
 // console.log(mFn);
+
+// 제이슨 데이터 불러오기
+import langData from './data_lang.json' with {type:'json'};
+
+console.log(langData);
+
+// 1. 다국어 요구사항
+// 언어 선택박스에서 언어를 변경하면 코드에 맞게 다국어 데이터를 제이슨
+// 파일에서 읽어와 본 페이지의 해당 데이터를 업데이트 한다
+
+// 2. 대상선정
+// 2-1. 이벤트 대상 : .sel
+const selBox =  mFn.qs('.sel');
+
+// 2-2. 변경 대상 : #gnb  a / #cont img / #info address
+// (1) GNB 메뉴 :  #gnb  a
+const gnbList = mFn.qsa('#gnb a');
+// (2) 메인 이미지 : #cont img
+const mainImg = mFn.qs('#cont img');
+// (3) 하단 주소 : #info address
+const addrBox = mFn.qs('#info address');
+
+
+// console.log(selBox,gnbList,mainImg,addrBox);
+
+
+//////////// 3. 이벤트 설정하기 ////////////////////
+// 이벤트 종류 : 선택박스가 변경될 때 발생하는 이벤트는? change
+mFn.addEvt(selBox,'change',chgLang);
+
+
+// 4. 함수 만들기
+function chgLang(){
+  // 1. 선택된 option의 value 속성값
+  let optVal = this.value;
+
+  // 2. 읽어온 option의 value값으로 다국어 객체 값 매칭하기
+  let selLang = langData[optVal];
+
+  // 호출 및 값 확인
+  console.log('변경',optVal,selLang);
+
+  // 3. 데이터 세팅하기
+  // 3-1. gnb 메뉴 세팅하기
+  gnbList.forEach((ele,idx)=>{
+    ele.innerText = selLang['메뉴'][idx];
+  }); /////// for each ////////////////////
+
+
+} /////////////////////// chgLang 함수 ////////////////////////
+
