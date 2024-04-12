1ae96090fd1245ea5cae4cf3541c23bc9ca1dd9b
doyeun93 <doyeun93@naver.com>
2024-04-12 13:20:02 +0900

    s

--- 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/main.js
+++ 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/main.js
@@ -1,79 +1,34 @@
 //// 도깨비 pj 메인 js - main.js ////
 
+// 공통처리 함수 불러오기 : 가장 먼저 처리한다
+
+import setElement from "./common.js";
+setElement(); // 함수 호출
+
+
 // 나의 함수 불러오기
 import myFn from "./my_function.js";
 //  부드러운 스크롤 불러오기
 import { startSS, setScrollPos } from "./smoothScroll23.js";
 
-// 모듈로 호출된 js에서는 다른 외부 js를 import로 호출가능
-// import하려는 파일에서 반드시 함수, 변수 등을 export 해야함
-
-import slideFn from "./slide.js";
 
 ///// 데이터 세팅 불러오기
 // = import {previewData} from "../data/dkb_data.js";
 import * as dkbData from "../data/dkb_data.js";
 
-// GNB 메뉴 데이터 불러오기
-import gnbData from "../data/gnb_data.js";
-console.log(gnbData);
 
 /******************************************************
  ******************** 구현 코드 파트 ******************
  ******************************************************/
 
-// GNB 메뉴 코드 넣기
-// 대상 : .gnb
-// 데이터 : gnbData는 객체니까 배열용 map()메서드 못씀 -> Object 키배열로 변환해서 사용함
-// 그리고 이 객체의 key는 상위 메뉴이기도 함
-// Object.keys(객체) -> 해당객체의 속성명(키) 배열 생성
-console.log(Object.keys(gnbData));
 
-myFn.qs(".gnb").innerHTML = `
-  <ul>
-    ${Object.keys(gnbData)
-      .map(
-        (v) => `
-        <li>
-          <a href="#">${v}</a>
-          ${
-            // 서브메뉴 "없음"이면 빈 값. 아니면 서브메뉴 출력
-            // gnbData[키] -> 값을 가져옴
-            gnbData[v] == "없음"
-              ? ""
-              : `
-            <div class="smenu">
-            <div class="swrap">
-              <h2>${v}</h2>
-              <ol>
-              ${gnbData[v]
-                .map(
-                  (vSub) => `
-                <li>
-                  <a href="#">${vSub}</a>
-                </li>
-                `
-                )
-                .join("")}
-              </ol>
-            </div>
-          </div>
-            `
-          }
-        </li>
-      `
-      )
-      .join("")}
-  </ul>
-`;
 
 // 1. 부드러운 스크롤 호출
 startSS();
 
 // console.log('모듈로 메인 js 호출', document.querySelector('.top-menu'));
 
-// 2 .slideFn 슬라이드 기능함수 호출
-slideFn();
+
 
 // 3. 인트로 동영상 파트 클릭시 동영상태그 넣기
 // 이벤트 대상 === 변경 대상 : .intro-mv-img
@@ -237,10 +192,10 @@ introMv.onclick = () => {
     hcode += `
       <li>
         <div class="clip-mv-box">
-          <img src="./images/clip_img/1.jpg" alt="김고은. 능력있으神 공유에게 뜬금포 사랑고백 ′사랑해요′">
+          <img src="./images/clip_img/${v.idx}.jpg" alt="${v.subtit}">
         </div>
-          <h4>김고은. 능력있으神 공유에게 뜬금포 사랑고백 ′사랑해요′</h4>
-          <h3>도깨비1화</h3>
+          <h4>${v.subtit}</h4>
+          <h3>${v.title}</h3>
       </li>
     `;
   }); //////// foreach //////////////////
