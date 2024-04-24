aebac80fdbdb6d60ec5fd2b18d028d33db842cdb
doyeun93 <doyeun93@naver.com>
2024-04-24 11:25:02 +0900

    s

--- 003.JS학습/01.JS실험실/js/call_myself.js
+++ 003.JS학습/01.JS실험실/js/call_myself.js
@@ -24,3 +24,70 @@ hcode += "</ul>";
 
 // 대상에 코드넣기
 gbox.innerHTML = hcode;
+
+
+
+///// 갤러리 박스를 왼쪽으로 계속 움직이게하는 재귀호출함수 만들기
+
+
+// 움직일 대상 : .gbox ul
+let target = mFn.qsEl(gbox,'ul');
+
+
+// 기준값 업데이트 함수 : 윈도우 가로폭의 1/4 => li 하나 크기
+const updateCriteria = () => window.innerWidth/4;
+
+// 기준값 
+let criteria = updateCriteria();
+
+// 리사이즈시 업데이트
+mFn.addEvt(window,"resize",
+
+    ()=> {criteria = updateCriteria();
+    console.log('기준값 업데이트:',criteria);
+});
+
+
+console.log('기준값:',criteria);
+
+// 현재 translate 값
+let currVal = 0;
+
+
+/********************************************************************** 
+참고 링크 : https://developer.mozilla.org/en-US/docs/Web/CSS/translate
+
+**********************************************************************/
+function moveGallery(){
+    // 현재값 1씩 감소
+
+    target.style.translate = --currVal + "px";
+
+    // 하나 크기만큼 나가면 처리
+    // 기준값을 마이너스로 하고 소수점 아래는 버림
+    // Math.floor()소수점 아래 내림(버림) 함수
+    if(currVal == Math.floor(-criteria)){
+        // 1. 맨앞 li 맨뒤로 이동
+        // appendChild(맨앞 li)
+        target.appendChild(mFn.qsaEl(target,"li")[0]);
+
+
+        // 2. translate 값 초기화
+        target.style.translate =  "0px";
+
+        // 3. 하나 크기만큼 나가면 currVal값 초기화
+        
+        currVal = 0;
+
+
+    } ///////////////// if 문 /////////
+
+    
+    
+    // 재귀호출(타임아웃함수로 호출함)
+    setTimeout(moveGallery,10);
+
+} //////// moveGallery ///////////////
+
+
+setTimeout(moveGallery,2000);
