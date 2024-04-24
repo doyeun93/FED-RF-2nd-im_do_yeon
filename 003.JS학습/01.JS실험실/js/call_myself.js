3b9938f5122cbba16fe94fcf763d038243a222d6
doyeun93 <doyeun93@naver.com>
2024-04-24 12:00:12 +0900

    Update call_myself.js

--- 003.JS학습/01.JS실험실/js/call_myself.js
+++ 003.JS학습/01.JS실험실/js/call_myself.js
@@ -35,9 +35,11 @@ let target = mFn.qsEl(gbox,'ul');
 
 
 // 기준값 업데이트 함수 : 윈도우 가로폭의 1/4 => li 하나 크기
-const updateCriteria = () => window.innerWidth/4;
+// window.innerWidth/4
+const updateCriteria = () => mFn.qsaEl(target,"li")[0].offsetWidth;
 
-// 기준값 
+
+// 기준값(대상 li의 가로크기값)
 let criteria = updateCriteria();
 
 // 리사이즈시 업데이트
@@ -67,8 +69,8 @@ function moveGallery(){
     // 기준값을 마이너스로 하고 소수점 아래는 버림
     // Math.floor()소수점 아래 내림(버림) 함수
     if(currVal == Math.floor(-criteria)){
-        // 1. 맨앞 li 맨뒤로 이동
-        // appendChild(맨앞 li)
+        // 1. 맨앞 li 맨뒤로 이동 /  appendChild(맨앞 li)
+        // 맨 앞 li는 새로 구해와야함(계속 변경되기때문에)
         target.appendChild(mFn.qsaEl(target,"li")[0]);
 
 
@@ -76,18 +78,36 @@ function moveGallery(){
         target.style.translate =  "0px";
 
         // 3. 하나 크기만큼 나가면 currVal값 초기화
-        
         currVal = 0;
 
-
     } ///////////////// if 문 /////////
-
-    
     
     // 재귀호출(타임아웃함수로 호출함)
+    // stopSts 변수값이 false일 때만 실행하기
+    if(!stopSts)
     setTimeout(moveGallery,10);
 
 } //////// moveGallery ///////////////
+
+
+// 대상에 마우스 오버시 멈추고 아웃시 다시 흘러가게하기
+// 대상 : .gbox ->  gbox변수
+// 멈춤상태 변수
+let stopSts = false;
+
+// 1. 멈추기(mouseenter)
+mFn.addEvt(gbox,"mouseenter",()=>{
+    //  멈춤상태변수 true변경
+    stopSts = true;
+
+});
+// 2. 다시 흘러가기(mouseleave)
+mFn.addEvt(gbox,"mouseleave",()=>{
+    //  멈춤상태변수 false 변경
+    stopSts = false;
+    // 재귀호출함수 호출하기
+    moveGallery();
+});
 
 
 setTimeout(moveGallery,2000);
