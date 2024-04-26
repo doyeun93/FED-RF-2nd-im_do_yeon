9ea85473913e1674ba8c3b7c8e3cd43f817b9a6d
doyeun93 <doyeun93@naver.com>
2024-04-26 17:23:53 +0900

    s

--- 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/main.js
+++ 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/main.js
@@ -17,6 +17,10 @@ import { startSS, setScrollPos } from "./smoothScroll23.js";
 import * as dkbData from "../data/dkb_data.js";
 
 
+// 드래그 슬라이드 불러오기
+import setSlide from "./drag_slide.js";
+
+
 /******************************************************
  ******************** 구현 코드 파트 ******************
  ******************************************************/
@@ -184,7 +188,7 @@ introMv.onclick = () => {
   const clipBox = myFn.qs(".clip-box");
 
   // 5-2. 생성코드 변수
-  let hcode = `<ul>`;
+  let hcode = `<ul class="slide">`;
 
   // 데이터만큼 순회하여 li코드 만들기
   // 데이터: dkbData.clipData
@@ -204,3 +208,8 @@ introMv.onclick = () => {
   // 5-3. 화면 출력하기
   clipBox.innerHTML = hcode;
 })(); ////// 최신동영상 코드 랩핑 구역 종료 ////////////
+
+
+// 태그 구성 후 드래그 슬라이드 호출하기
+setSlide('banbx');
+
