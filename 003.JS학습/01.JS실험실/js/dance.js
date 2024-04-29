caee949fb14d8c135d24a671c2ccaf67c0e0187a
doyeun93 <doyeun93@naver.com>
2024-04-29 11:21:47 +0900

    s

--- 003.JS학습/01.JS실험실/js/dance.js
+++ 003.JS학습/01.JS실험실/js/dance.js
@@ -3,6 +3,8 @@
 // 나의 함수 불러오기
 import mFn from './my_function.js';
 
+// [1] 태그 세팅하기
+
 // 1. 3번 스테이지에 ul>li 구조 이미지 넣기
 
 // 대상 : .slidePg(스티기 박스)
@@ -27,5 +29,57 @@ hcode += "</ul>";
 // 3. 코드 출력하기
 slidePg.innerHTML = hcode;
 
+//////////////////////////////////////////////////////////
+
+// [2] 3번째 영역에 도달한 경우  ul박스 가로방향 이동하기
+
+// 1. 대상선정하기
+// 이벤트 대상 : window
+// 이벤트 종류 : scroll 
+// 위치기준대상 : .tpg -> 스티키를 싸고 있는 부모박스
+const tpg =  mFn.qs('.tpg');
+
+// 움직일 대상 : .slidePg>ul
+const target = mFn.qs('.slidePg>ul');
+
+// 추가 대상 : .slidePg>ul>li
+const eachList = mFn.qsaEl(target,'li');
+
+  
+// console.log(tpg,target,eachList);
 
 
+// 2. 이벤트 설정하기
+mFn.addEvt(window,'scroll', moveSlide);
+
+// 3. 함수 만들기
+function moveSlide(){
+  // 1. 스티키 부모박스 바운딩 top값
+  let bTop = mFn.getBCR(tpg);
+  // console.log('바운딩top:', bTop);
+
+  // 2. 이동할 타겟박스 left값으로 부모 바운딩 top값 넣기
+  // (1) 바운딩 top값이 0초과일때 처음 위치 재설정하기
+  if(bTop > 0){
+    target.style.left = '0px';
+  }
+  // (2) 바운딩 top값이 0이하 -3000이하일때 부모 바운딩 top값 위치이동하기
+  else if(bTop <= 0 && bTop >= -3000){
+    target.style.left = bTop + 'px';
+  }
+  // (3) 마지막 한계 이후엔 한계값으로 세팅
+  else{
+    target.style.left ='-3000px';
+  }
+
+  // 3. 스티키 박스 하위 li가 보이는 화면 left 기준선에 대한 위치값
+  // 바운딩 left값을 이용하여 자기 위치에서 위/아래로 이동하는 수치변경하기
+  
+  let mVal = mFn.getBCR2(eachList[2]);
+  // 위치값을 양수로 만들고 윈도우 크기로 나눈 후 백분율. 수치가 크므로 2로 나움
+  mVal = Math.abs(mVal / window.innerWidth)*100/2;
+  console.log('바운딩left:',mVal);
+
+  eachList[2].style.translate = `0 ${mVal}%`;
+
+} ///////// moveSlide 함수 ////////////////////
