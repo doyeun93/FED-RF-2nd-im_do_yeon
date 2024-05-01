1a06a0c05d509cb027987686df707651a85c639f
yjhong <qbru@naver.com>
2024-05-01 16:17:45 +0900

    Update 06.Event.jsx

--- 004.React학습/01.react-cdn/js/06.Event.jsx
+++ 004.React학습/01.react-cdn/js/06.Event.jsx
@@ -21,11 +21,74 @@ function EventShow(){
 
 // 컴포넌트 리턴 코드 위에서 이벤트 처리를 위한 
 // 함수를 만들어서 사용할 수 있다 ->> 지역함수로 사용됨
+
+// 오버시 이벤트 한번만 실행되게 상태변수만들기
+let onceSts = false;
+
 // 1. 컴포넌트 내부 함수 
 // (1) 소원이 무엇이냐 물어보는 함수
 const showAladin = () => {
+    // 1. 한번만 실행 분기문
+    if(onceSts) return;
+    onceSts = true; // 한번만 실행
+
+
     console.log('알라딘이 누구야?');
+
+    // 2. 알라딘 이미지 출력하기
+    // (1) html 출력대상 : #ala
+    let alaBox = mFn.qs("#ala");
+
+    // (2) 이미지출력
+    ReactDOM.render(<img src="./images/ala4.jpg" alt="알라딘"/> ,alaBox);
+
+    // 3. 말풍선 박스에 글자넣기
+    let titBox = mFn.qs(".tit");
+    titBox.innerText = "소원이 무엇이냐?";
+
+    // 4. 말풍선 박스에 인라인 CSS 코드 넣기
+    titBox.style.cssText = ` 
+        width: 70%;
+        padding: 50px 0;
+        margin: 0 auto;
+        border: 2px solid lime;
+        opacity: 0;
+    `;
+
+    // 5. 0.5초 후 CSS변경으로 타이틀 등장하기
+    setTimeout(() => {
+        let tg = titBox.style;
+        tg.opacity = 1;
+        tg.transition = " 2s ease-in-out 1s";
+        tg.borderRadius = "50%";
+        tg.translate = "0 -200px";
+        tg.fontSize = "40px";
+        tg.color = "white";
+        tg.backgroundColor = "rgba(0, 0, 0, .5 )";
+    },500);
+
+
+    // 6. 램프 가져오기 버튼 3초후 보이기
+    setTimeout(() => {
+       mFn.qsa("button")[0].style.display = "inline-block";
+    }, 3000);
+
+    // 7. 램프 가져오기 버튼 3초후 보이기
+    setTimeout(() => {
+        mFn.qsa("button")[1].style.display = "inline-block";
+     }, 3500);
+
+
 }; ////// showAladin 함수 
+
+
+// (2) 램프 가져오기 함수 ////////////////////////
+const getLamp = () => {
+    console.log('램프가져와~!');
+
+}; /// getLamp 함수 /////////////
+
+
 
 
 // 2. 리턴 코드 만들기 -> 반드시 html을 return 해줘야함
@@ -38,6 +101,17 @@ const showAladin = () => {
                 // 마우스 오버시 showAladin 함수 호출
                     onMouseOver={showAladin}
                 />
+                {/* 램프가 들어갈 요소 */}
+                <div className="lamp"></div>
+                
+                {/* 버튼들 */}
+                <button onClick={getLamp}>램프가져오기~!</button>
+                <button>소원빌기! 페라리 주세요!!</button>
+
+                {/* 소원이 무엇이냐 말풍선 박스 */}
+                <div className="tit">
+                    
+                </div>
             </div>
         </React.Fragment>
     );
