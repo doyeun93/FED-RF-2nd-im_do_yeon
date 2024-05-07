8e8f4f52c5d158b4d408d8e55456b04ab346392f
yjhong <qbru@naver.com>
2024-05-07 16:14:19 +0900

    Update 07.조건렌더링.jsx

--- 004.React학습/01.react-cdn/js/07.조건렌더링.jsx
+++ 004.React학습/01.react-cdn/js/07.조건렌더링.jsx
@@ -80,3 +80,77 @@ ReactDOM.render(
 ReactDOM.render(
     <Developer isNormal={true} isrc={devImg[1]} ialt= "일반인 동석" itit="개발자가 뭡니까?" />, 
     root[1]);
+
+
+
+/******************************************************** 
+    2. if문이 아닌 조건 표현하기
+    -> 조건식 && JSX표현식
+    조건이 true일때만 && 뒤의 JSX표현식이 출력됨!
+    -> 조건식은 true / false 의 결과가 나오는 식이며
+    AND 조건, OR조건, NOT조건식이 모두 사용가능함
+    예) A == "사자" && (B > 10 || C == "별" || !D) 
+*********************************************************/ 
+
+// 개발자의 취향을 알아보자
+
+// 2-1. 제목을 찍기 위한 타이틀 컴포넌트 
+function SetTitle({title}){
+    return <h1>👻개발자😊가 좋아하는 {title}</h1>
+
+} ////// setTitle 컴포넌트 
+
+
+// 음식리스트
+const foods = 
+["스파게티","라면","짜글이","짜장면","마라탕"];
+
+
+// 2-2. 반복리스트를 위한 컴포넌트
+function FoodList({foodName}){
+    return <li> 개발자는 {foodName} 좋아해 </li>;
+
+} ////// FoodList 컴포넌트 /////////////////
+
+
+// 2-3. 개발자 선호 음식리스트 출력 컴포넌트 /////////
+function WishList({wList}){
+    // wList - 좋아하는 음식
+    // 리턴 코드 구역
+    return(
+        <React.Fragment>
+            {/* 음식 리스트 타이틀 */}
+            <SetTitle title="음식" />
+            {
+                wList.length > 0 && (
+                <div>
+                    <h2> 개발자가 좋아하는 음식은 모두 {wList.length}가지 입니다
+                    </h2>
+                    <ul>
+                        {
+                        // 배열 변수.map() 메서드 사용
+                        // map()메서드는 원래 새로운 배열을 현재 자리에 출력하는 용도임
+                        // 그러나 리액트는 이것을 변경하여 표현식안에서 출력시 
+                        // 태그 JSX 형식으로 변환해 줌! js처럼 map.join('') 처리 불필요
+                        wList.map(v => <FoodList foodName={v}/>)
+                        }
+                    </ul>
+                </div>
+                )
+            }
+            {/* 배열값이 0인 경우 다른것 출력하기 */}
+            {
+                wList.length == 0 && 
+                <h2>아직 개발자 음식 리스트가 업데이트 되지 않았습니다.</h2>
+            }
+        </React.Fragment>
+
+    );
+
+} /////// WishList 컴포넌트 /////////
+
+// 컴포넌트 출력 : 배열값 있는 경우
+ReactDOM.render(<WishList wList={foods}/>, root[2]);
+
+// 컴포넌트 출력 : 배열값 없는 경우
+ReactDOM.render(<WishList wList={[]}/>, root[3]);
