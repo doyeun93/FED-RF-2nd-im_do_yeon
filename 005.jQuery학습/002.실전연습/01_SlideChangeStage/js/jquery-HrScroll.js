4121c508a6ba50110e202f6638b0ca997626b8e3
doyeun93 <doyeun93@naver.com>
2024-05-16 12:37:31 +0900

    s

--- 005.jQuery학습/002.실전연습/01_SlideChangeStage/js/jquery-HrScroll.js
+++ 005.jQuery학습/002.실전연습/01_SlideChangeStage/js/jquery-HrScroll.js
@@ -7,6 +7,9 @@ const scTarget = $("html, body");
 // 제이쿼리에서 전체 스크롤/ 휠 이벤트 작성시 대상은 항상 html, body로 두개 모두 잡아준다
 // 참고로 document나 window는 사용안됨
 // 이벤트 메서드 : on(이벤트명, 함수)
+// 유의사항 : 휠 이벤트는 모바일과 무관하다. 따라서 모바일 터치 가로 스크롤은
+// 그냥 가로 스크롤바만 살려주면 자연스럽게 된다
+
 
 // 스크롤 위치값변수
 let scPos = 0;
@@ -18,14 +21,20 @@ let pgCnt = $(".page").length;
 // maxLimit : 최대한계값
 let winW, maxLimit;
 
+
+// 한번에 움직일 스크롤 이동크기
+// 상수는 모두 대문자로 쓰고 중간에 언더바로 구분한다
+const MOVE_NUM = 100;
+
+
 const chgLimit = () => {
     winW = $(window).width();
+
     // 최대한계값 : 전체 이동 박스 크기 - 화면 가로 크기
     maxLimit = (winW*pgCnt) - winW;
 
-    console.log("window크기:",winW, "/페이지수:",pgCnt,
-    "/최대한계값:",maxLimit);
-    // 함수내에서 호출 후  리사이즈 이후 또 호출하게끔(리사이즈 업데이트할 때마다 호출됨)
+    console.log("window크기:",winW, "/페이지수:",pgCnt, "/최대한계값:",maxLimit);
+    // 함수내에서 호출 후 리사이즈 이후 또 호출하게끔(리사이즈 업데이트할 때마다 호출됨)
 
 } /// chgLimit 함수 ///
 
@@ -37,6 +46,10 @@ $(window).resize(chgLimit);
 // resize()메서드 : 리사이즈 이벤트 함수
 
 
+//////////////////////////////////
+/// 스크롤 이벤트 함수 구현하기 ///
+/////////////////////////////////
+
 
 scTarget.on("wheel",(e) => {
     // 스크롤 이동을 위한 제이쿼리 속성
@@ -44,10 +57,19 @@ scTarget.on("wheel",(e) => {
     // 2. scrollLeft : 가로 스크롤 바 위치
 
     // 휠 방향 알아내기(전체 이벤트 객체로부터 얻어옴)
-    let delta = event.wheelDelta;
+    // (1) event.wheelDelta 값 : 기본 이동 100px + 앞뒤 예비 공백 10px*2=20px
+    // 전체 표시 수치 120 -> 120px을 의미 
+    // let delta = event.wheelDelta;
+
+    // (2) event.deltaY (y는 기본브라우저 휠이 위아래로 내리기때문에)
+    // 기본 이동 크기만 표시함 즉, 100PX 이동값 표시
+    // 방향은 양수가 아랫방향 -> wheelDelta보다 나중에 나온 실질적인 표시객체임
+    let delta = event.deltaY;
     
-    if(delta<0) scPos += 200;
-    else scPos -= 200;
+    // 방향에 따를 증감은 deltaY는 양수가 아랫방향
+    // wheelDelta는 음수가 아랫방향
+    if(delta > 0) scPos += MOVE_NUM;
+    else scPos -= MOVE_NUM;
 
     // 한계값 체크
     // (1) 최소 한계 : 0
