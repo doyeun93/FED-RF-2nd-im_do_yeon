bc27375a988a5d28fa022f0f4067bb02e8800b2f
doyeun93 <doyeun93@naver.com>
2024-05-17 15:16:15 +0900

    s

--- 004.React학습/02.react-ex/js/01.공유신발.jsx
+++ 004.React학습/02.react-ex/js/01.공유신발.jsx
@@ -19,7 +19,8 @@ function MainComponent() {
   // 최상위 컴포넌트에서만 Hook을 호출한다.
   // 1. 리스트 / 상세보기 전환용 상태관리 변수
   const [ viewList, setViewList] = React.useState(true);
-  
+  // 2. 상품 데이터 인덱스값 상태관리 변수
+  const [idx, setIdx] = React.useState(0); 
 
 
   /************************************** 
@@ -56,7 +57,7 @@ function MainComponent() {
       <div className="gwrap">
         {
           // 상태 관리 변수 viewList값이 true이면 리스트보기
-          viewList?<GoodsList viewDetail={setViewList}/>:<GoodsDetail backList={setViewList}/>
+         viewList?<GoodsList viewDetail={setViewList} updateIdx={setIdx}/>:<GoodsDetail backList={setViewList} gNo={idx} />
         }  
       </div>
     </React.Fragment>
@@ -66,16 +67,15 @@ function MainComponent() {
 
 // [상품 리스트 서브 컴포넌트(서브 컴포넌트는 개별 컴포넌트) : GoodsList] //
 // map()메서드는 표현식이라 중괄호 필요
-function GoodsList({viewDetail}){
+function GoodsList({viewDetail, updateIdx}){
   // viewDetail : 부모 컴포넌트가 전달해준 상태변수 viewList를 업데이트하는 setViewList메서드임
-
+  // updateIdx : 부모 컴포넌트의 setIdx 상태관리 변수 메서드 
   return(
   <ul>
    { 
   // 반복요소 li에 key 속성을 쓸 것을 리액트는 필수적이라고 함
   // 어디에 쓰냐? 업데이트시 순번구분을 위함
-  // node.js 개발환경에서 안쓰면 에러남 
-   
+  // node.js 개발환경에서 안쓰면 에러남  
    guData.map((v,i) => (
       <li key={i}>
         <a href="#" onClick={(e)=> {
@@ -83,7 +83,10 @@ function GoodsList({viewDetail}){
           e.preventDefault(); 
           // 상태변수 viewList 업데이트
           // setViewList메서드가 viewDetail로 들어옴
-          viewDetail(false) }} > 
+          viewDetail(false);
+          // setIdx메서드가 updateIdx로 들어옴
+          updateIdx(i);
+          }} > 
     {/* onClick={viewDetail(false)} -> 이렇게쓰면 바로 실행되어 상품 리스트는 사라지고 바로 
     상세리스트가 보이므로 익명함수 " ()=> "를 사용하면 상품리스트 클릭시 상세리스트로 이동한다 */}
           <ol className="glist">
@@ -103,25 +106,26 @@ function GoodsList({viewDetail}){
 
 
 ////// [상품 상세보기 서브 컴포넌트 : GoodsDetail]
-function GoodsDetail({backList}){
-  // backList : 부모 컴포넌트가 전달해준 상태변수 viewList를 업데이트하는 setViewList메서드임
-
+function GoodsDetail({backList, gNo}){
+  // backList : 부모 컴포넌트가 전달해준 상태변수 (viewList를 업데이트하는 setViewList메서드임)
+  // gNo : 상품 데이터 배열순번
+  // (idx 상태관리변수가 전달됨 - 이 값 변경시 컴포넌트 변경됨)
   return(
     <ol style={{display:"flex", listStyle:"none", justifyContent:"center"}}>
           <li>
-            <img src="./images/vans/vans_1.jpg" alt="반스신발" style={{width:"100%"}}/>
+            <img src={"./images/vans/vans_"+guData[gNo].idx+".jpg"} alt="반스신발" style={{width:"100%"}}/>
           </li>
           <li style={{lineHeight:"2", padding:"10px", textAlign:"left"}}>
-            상품명 : {guData[0].gname}<br/>
-            가격 : {guData[0].gprice}<br/>
-            소재 : {guData[0].소재}<br/>
-            색상 : {guData[0].색상}<br/>
-            치수 : {guData[0].치수}<br/>
-            제조자/수입자 : {guData[0]["제조자/수입자"]}<br/>
-            제조국 : {guData[0].제조국}<br/>
-            제조연월 : {guData[0].제조연월}<br/>
-            A/S 책임자와 전화번호 : <br/>{guData[0]["A/S 책임자와 전화번호"]}<br/>
-            Model : {guData[0].Model}<br/>
+            상품명 : {guData[gNo].gname}<br/>
+            가격 : {guData[gNo].gprice}<br/>
+            소재 : {guData[gNo].소재}<br/>
+            색상 : {guData[gNo].색상}<br/>
+            치수 : {guData[gNo].치수}<br/>
+            제조자/수입자 : {guData[gNo]["제조자/수입자"]}<br/>
+            제조국 : {guData[gNo].제조국}<br/>
+            제조연월 : {guData[gNo].제조연월}<br/>
+            A/S 책임자와 전화번호 : <br/>{guData[gNo]["A/S 책임자와 전화번호"]}<br/>
+            Model : {guData[gNo].Model}<br/>
             <div className="btnbx" style={{textAlign:"right", padding:"15px"}}>
             <button onClick={()=>backList(true)}
             style={{fontSize:"24px"}}>리스트로 가기</button>
