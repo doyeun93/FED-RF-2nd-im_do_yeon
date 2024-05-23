4cfef59706561a7a486f4b3f8e72bd69beb13f30
doyeun93 <doyeun93@naver.com>
2024-05-23 17:21:57 +0900

    Update js9-3.local_storage.js

--- 003.JS학습/JS/js9-3.local_storage.js
+++ 003.JS학습/JS/js9-3.local_storage.js
@@ -194,8 +194,9 @@ function bindData(){
 
 } //////// bindData 함수//////////////////////////
 
-// 게시판 최초 호출
-bindData();
+// 게시판 최초 호출 : 로컬쓰 minfo 존재여부에 따라 처리
+if(localStorage.getItem("minfo")) bindData();
+else makeObj();
 
 
 /// 게시판 입력 버튼 클릭시 구현하기
@@ -207,17 +208,53 @@ mFn.qs("#sbtn").onclick=()=>{
     
     console.log("입력!", localData);
 
-    // 2. 입력할 데이터 객체 형식으로 배열에 넣기 : 배열.push({객체})
+    // 2. 입력값이 비었으면 돌려보내기
+    // trim() : 글자사이 공백은 가능하지만 그냥 앞뒤 공백 제거해주는 메서드
+    if(mFn.qs("#tit").value.trim() == "" 
+    || mFn.qs("#cont").value.trim() == "") {
+      alert("제목과 내용을 입력하세요");
+      return;
+    } /// if
+
+
+console.log("idx값 배열:", localData.map(v=>v.idx));
+
+    // 3. 입력할 데이터 객체 형식으로 배열에 넣기 : 배열.push({객체})
     localData.push({
-        idx: localData.length,
+      // 순번은 배열객체 idx 값 중 최대값을 구하여 1 더한다
+      // apply(보낼객체, 배열) -> 보낼 객체가 여기서는 null
+        idx: Math.max.apply(null,localData.map(v=>v.idx))+1,
         tit: mFn.qs("#tit").value,
         cont: mFn.qs("#cont").value
     });
 
-    // 3. 배열 데이터를 문자화 하여 로컬쓰에 입력
+    // 4. 배열 데이터를 문자화 하여 로컬쓰에 입력
     localStorage.setItem("minfo",JSON.stringify(localData));
 
-    // 4. 화면 출력 함수 호출하기
+    // 5. 화면 출력 함수 호출하기
     bindData();
 
 }; //////// click 함수 //////////
+
+
+//// CRUD: Create/ Read / Update / Delete
+
+
+
+//////// 수정 기능 구현하기 ///////
+
+// 수정항목 선택박스 업데이트 함수 호출
+updateItemList();
+
+// 수정할 항목 업데이트 함수
+function updateItemList(){
+  // 대상 : 수정선택박스 - #sel
+  const selBox = mFn.qs("#sel");
+
+  // 데이터의 idx를 순회하며 option 만들기
+  const localData = JSON.parse(localStorage.getItem("minfo"));
+
+  selBox.innerHTML = localData.map(v=>`
+    <option value = "${v.idx}">${v.idx}</option>
+  `).join('');
+}
