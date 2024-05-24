f83037a1a8accca63429fcc539e1d7703501f2d0
doyeun93 <doyeun93@naver.com>
2024-05-24 17:20:41 +0900

    s

--- 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/items.js
+++ 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/items.js
@@ -21,7 +21,7 @@ function Layout(){
        {/* 1. 상단영역 컴포넌트 */}
       <TopArea/>
        {/* 2. 메인영역 컴포넌트 */}
-      <ItemsArea/>
+      <ItemsArea catName="living"/>
        {/* 3. 하단영역 컴포넌트 */}
       <FooterArea/>
 
