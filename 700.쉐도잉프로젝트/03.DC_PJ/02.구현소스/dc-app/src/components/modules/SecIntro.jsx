f37086a210ecbbc310f52e6673482484870ce3cd
doyeun93 <doyeun93@naver.com>
2024-06-07 15:28:06 +0900

    S

---
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/SecIntro.jsx
@@ -0,0 +1,19 @@
+// DC.com 섹션 소개 모듈 - SecIntro.jsx
+import React from 'react';
+
+// 섹션 소개 모듈 데이터 가져오기
+import { secIntroData } from '../data/sec_intro';
+
+
+// 섹션 소개 모듈 CSS 불러오기
+import "../../css/sec_intro.scss";
+
+function SecIntro(props) {
+    return (
+        <div>
+            섹션 소개 모듈
+        </div>
+    );
+}
+
+export default SecIntro;
