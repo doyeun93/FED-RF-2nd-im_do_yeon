6715f52d7504917504185c60c101ed936f77c6ae
doyeun93 <doyeun93@naver.com>
2024-06-10 13:19:56 +0900

    s

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/VidIntro.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/VidIntro.jsx
@@ -15,6 +15,25 @@ function VidIntro({ catName, clsName }) {
   // 선택 데이터 변수 할당
   const selData = vidIntroData[catName];
 
+  // 링크 코드 만들기 함수
+  const linkCode = (v) => {
+    // *로 자른 후 배열로 변환
+    let data = v.split("*");
+    // 코드 리턴 구역 ////
+   return(
+
+    <>
+    {data[0]}
+    <a href={selData.link[1]}
+        target="_blank"
+    >{selData.link[0]}</a> 
+    {data[1]};
+
+    </>
+   ); 
+
+  };
+
   return (
     <section className={"vidbox " + clsName}>
       <div>
@@ -30,9 +49,9 @@ function VidIntro({ catName, clsName }) {
           {/* 큰 제목 */}
           <h2>{selData.btit}</h2>
           {/* 요약소개(링크포함) : sum */}
-          <p>{selData.sum}</p>
+          <p>{selData.sum.indexOf("*") == -1 ? selData.sum : linkCode(selData.sum)}</p>
           {/* 설명(링크포함) : desc */}
-          <p>{selData.desc}</p>
+          <p>{selData.desc.indexOf("*") == -1 ? selData.desc : linkCode(selData.desc)}</p>
         </div>
       </div>
     </section>
