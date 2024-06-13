c01238d1722081e2bfbd0fc97cc0da8af8dad681
doyeun93 <doyeun93@naver.com>
2024-06-13 15:22:39 +0900

    s

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/CatList.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/CatList.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 // 캐릭터 리스트 CSS
 import "../../css/cat_list.scss";
+import { SwiperCat } from "../plugin/SwiperCat";
 
 // DC PJ 캐릭터 리스트 컴포넌트
 /* 
@@ -24,6 +25,7 @@ function CatList(props) {
       {/* 1. 모듈타이틀 */}
       <h2 className="tit">WHO'S WHO: THE JUSTICE LEAGUE</h2>
       {/* 2. 스와이퍼 컴포넌트 */}
+      <SwiperCat />
     </section>
   );
 }
--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/plugin/SwiperCat.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/plugin/SwiperCat.jsx
@@ -19,55 +19,24 @@ import "swiper/css/navigation";
 import "./css/swiper_cat.scss";
 
 // 데이터 불러오기
-import { swVidData } from "../data/swiper_vid";
+import { catListData } from "../data/swiper_cat";
 
 // import required modules
 // 사용할 스와이퍼 모듈을 불러온다
 // ( 여기서는 네비게이션 - 양쪽 이동 버튼 )
 import { Navigation } from "swiper/modules";
 
-export function SwiperCat({ catName }) {
-  // catName : 카테고리명
+export function SwiperCat() {
 
   // 선택데이터 변수 할당
-  const selData = swVidData[catName];
+  const selData = catListData;
 
 
 
-  // 비디오 보이기 함수 ///
-  const showVideo = (src,tit) => {
-    console.log("비디오 보여", src, tit);
-
-    // 1. 대상선정
-    // 1-1. 아이프레임 : .play-vid iframe
-    const ifr = $('.play-vid iframe');
-    // 1-2. 전체 박스 : .vid-bx
-    const vbx = $('.vid-bx');
-    // 1-3. 타이틀 박스 : .ifr-tit
-    const itit = $('.ifr-tit');
-    // 1-4. 닫기 버튼 : .cbtn
-    const cbtn = $('.cbtn');
-
-    // 2. 변경하기
-    // 2-1. 아이프레임 src 경로 넣기
-    ifr.attr("src",src+"?autoplay=1");
-    // 2-2. 비디오 타이틀 넣기
-    itit.text(tit);
-    // 2-3. 박스 보이기
-    vbx.fadeIn(300);
-    // 2-4. 닫기버튼 세팅
-    cbtn.on("click", () =>{
-      // 전체박스 사라지기
-      vbx.fadeOut(300);
-      // 기존 동영상 플레이 멈추기(src 값 삭제)
-      ifr.attr("src","");
-    } );
+  
 
 
-  }; ////// showVideo 함수 ///////////////
-
-
-
+// 코드 리턴 구역
 
   return (
     <>
@@ -81,44 +50,34 @@ export function SwiperCat({ catName }) {
         // 스와이퍼 사이즈별 슬라이드수 변경!
         breakpoints={{
           200: {
-              slidesPerView: 1,
-          },
-          500: {
-              slidesPerView: 2,
-          },
-          1000: {
               slidesPerView: 3,
           },
-          1200: {
+          700: {
               slidesPerView: 4,
           },
+          1000: {
+              slidesPerView: 5,
+          },
+          1200: {
+              slidesPerView: 7,
+          },
         }}
-        className="mySwiper"
+        className="mySwiper2"
       >
         {selData.map((v, i) => (
+          // idx 고유번호가 7번 이하만 출력 
+          // idx가 문자형숫자 이므로 비교를 위해 숫자형 변환해줌 -> Number(변수)
+          Number(v.idx) <= 7 &&
           <SwiperSlide key={i}>
-            <section className="sw-inbox" onClick={()=>showVideo(v.vsrc,v.tit)}>
-              {/* 동영상이미지박스 */}
-              <div className="vid-img">
-                <img src={v.isrc} alt={v.tit} />
-                {/* 폰트어썸 아이콘 */}
-                <FontAwesomeIcon style={{
-                        position:'absolute',
-                        bottom: '55%',
-                        left:'10%',
-                        color:'#fff',
-                        fontSize:'50px'
-                      }} 
-                 icon={faCirclePlay} 
-                 />
-               
-               
-              </div>
-              {/* 동영상 타이틀 박스 */}
-              <div className="vid-tit">
-                <h4>{v.cat}</h4>
-                <h3>{v.tit}</h3>
-              </div>
+            <section className="sw-inbox2">
+                  {/* 캐릭터이미지영역 */}
+                  <div className="cat-img2">
+                    <img src={v.tmsrc} alt={v.cname} />
+                  </div>
+                  {/* 캐릭터타이틀영역 */}
+                  <div className="cat-tit2">
+                    <h3>{v.cname}</h3>
+                  </div>
             </section>
           </SwiperSlide>
         ))}
