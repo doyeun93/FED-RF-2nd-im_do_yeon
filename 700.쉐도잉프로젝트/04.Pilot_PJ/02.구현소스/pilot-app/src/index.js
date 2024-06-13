ba85b6c01826a1b654aff3b78dd7f482beb185b9
doyeun93 <doyeun93@naver.com>
2024-06-13 12:21:49 +0900

    s

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/index.js
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM, { createRoot } from 'react-dom/client';
+import TopArea from './components/layout/TopArea';
+import MainArea from './components/layout/MainArea';
+import FooterArea from './components/layout/FooterArea';
+import './css/index.scss';
+
+// 전체 공통 css
 
 function MainComponent(props) {
   return (
-    <div>
-      <h1>파일럿 PJ</h1>
-    </div>
+    <>
+      <TopArea />
+      <MainArea />
+      <FooterArea />
+    </>
   );
 }
 
