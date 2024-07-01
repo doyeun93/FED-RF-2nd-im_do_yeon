ce936def96372fe8e898d7431a2f1e6f8090b395
doyeun93 <doyeun93@naver.com>
2024-07-01 15:49:47 +0900

    s

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Member.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Member.jsx
@@ -87,7 +87,7 @@ function Member(props) {
     // 유효성 검사방법 : 정규식.test(값)
 
     // 2. 입력값 확인 : e.target
-    console.log(val);
+    // console.log(val);
 
     // 3. 에러상태 분기하기
     // 3-1. 에러 아닐 때(유효성 검사만 통과한 경우)
@@ -104,19 +104,47 @@ function Member(props) {
         // 2. 로컬스 객체 변환(-> 문자형이므로 객체변환 필요)
         memData = JSON.parse(memData);
         // -> 배열 데이터로 변환
+        // 주의 : JSON 파싱할 때 원본 형식이 제이슨 파일 형식으로 엄격하게 작성되어야 에러가 없음(마지막 , 불허용 등)
 
         // 3. 배열이니까 현재 입력데이터의 아이디가 기존 배열 값으로 있는지 검사함
         // -> 있으면 true, 없으면 false
-        console.log(memData);
+        
+        // [새로운 배열 메서드 : some()]
+        // ->  조건에 맞는 값이 하나만 나오면 true 처리함
+        // 비교참고) every()는 하나만 false이면 false 리턴
+        // let isT = memData.some(v=> {
+        //     console.log("돌아",v.uid);
 
-
+        //     return v.uid === val});
+        
+        
+        let isT = memData.some((v) => v.uid === val);
+        
+        console.log("중복있어?",isT);
+        // 4. true일 경우 중복데이터 메시지 표시 
+        if(isT){
+            // 에러메세지 업데이트
+            setIdMsg(msgId[1]);
+            // 에러상태값 업데이트
+            setUserIdError(true);
+            
+        } /// if ////
+        // 5. false일 경우 성공 메시지 표시
+        else{
+        
+            // 에러상태값 업데이트 : 에러가 아님(false)
+            setUserIdError(false); 
+        }
         // 아이디 에러상태 업데이트(false)
-        setUserIdError(false);
+        // setUserIdError(false);
     }
     
-    // 3-2. 에러일 때
+    // 3-2. 에러일 때 : 유효성 검사 에러
     else{
         console.log("에러");
+        
+        // 에러메세지 업데이트
+        setIdMsg(msgId[0]);
         // 아이디 에러상태 업데이트(true)
         setUserIdError(true);
     } // else
@@ -141,10 +169,22 @@ function Member(props) {
               {
               /* 에러일 경우 메시지 출력 */
               // 조건문 && 출력요소
-              userIdError && 
+              (userIdError && userId) &&(
               <div className="msg">
-                <small style={{color:"red", fontSize:"10px"}}>{idMsg}</small>
+                <small style={{color:"red", fontSize:"11px"}}>{idMsg}</small>
               </div>
+              )
+              }
+              {
+              /* 통과시 메시지 출력 */
+              // 조건문 && 출력요소
+              // 조건 추가 : userId가 입력 전일 때 안보임처리
+              // userId가 입력전엔 false로 리턴됨
+                (!userIdError && userId) &&(
+                <div className="msg">
+                    <small style={{color:"green", fontSize:"11px"}}>{msgId[2]}</small>
+                </div>
+                )
               }
 
             </li>
