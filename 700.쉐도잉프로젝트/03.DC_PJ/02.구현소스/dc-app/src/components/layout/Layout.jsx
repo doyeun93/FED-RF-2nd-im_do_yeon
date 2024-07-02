ce3fb23c12f1ae59f5bd8663d0c9326274a3010b
doyeun93 <doyeun93@naver.com>
2024-07-02 15:18:26 +0900

    s

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/layout/Layout.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/layout/Layout.jsx
@@ -4,24 +4,39 @@ import { useState } from "react";
 import FooterArea from "./FooterArea";
 import MainArea from "./MainArea";
 import TopArea from "./TopArea";
+import { useNavigate } from "react-router-dom";
 
 // 컨텍스트 API 불러오기
 import { dCon } from "../modules/dCon";
 
 export default function Layout(){
     // 상태관리 변수 ///
-    // 1. 로그인 상태관리 변수 -> 초기값으로 로컬스토리지 "minfo"를 할당함
-    const [loginSts, setLoginSts] = useState("나야나");
-    // useState(localStorage.getItem("minfo"));
+    // 1. 로그인 상태관리 변수 -> 초기값으로 세션스토리지 "minfo"를 할당함
+    const [loginSts, setLoginSts] = 
+    useState(sessionStorage.getItem("minfo"));
 
     // 2. 로그인 환영 메시지 상태변수
     const [loginMsg, setLoginMsg] = useState(null);
+    console.log(loginMsg);
+
+    // [ 공통 함수]
+    // 1. 라우팅 이동 함수
+    const goPage = useNavigate();
+    // 2. 로그인 환영메시지 생성함수
+    const makeMsg = (name) => {
+        // 유저아이콘
+        let usrIcon = ["🙍‍♂","🧏‍♀","🦸‍♂","👨‍🎤","🦸‍♀"];
+        // 랜덤수 : 0~4사이의 수
+        let rdm = Math.floor(Math.random() *5);
+        // 로그인 메시지 상태변수 업데이트
+        setLoginMsg(`Welcome ${name} ${usrIcon[rdm]}`);
+    }; ////// makeMsg 함수 ///////////
 
 
     //// 코드 리턴 구역
     return(
         // Provider value 속성으로 전역노출 변수를 설정함
-        <dCon.Provider value={{loginSts, setLoginSts,setLoginMsg}}>
+        <dCon.Provider value={{loginSts, setLoginSts,setLoginMsg,goPage,makeMsg,}}>
         {/* 1. 상단영역 */}
         <TopArea/>
         {/* 2. 메인영역 */}
