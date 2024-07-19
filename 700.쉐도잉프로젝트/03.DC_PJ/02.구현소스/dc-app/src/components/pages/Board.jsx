f6ee3a8b9a1f26055a541b9164993f289b4c10c6
doyeun93 <doyeun93@naver.com>
2024-07-19 15:42:37 +0900

    Update Board.jsx

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Board.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Board.jsx
@@ -747,7 +747,7 @@ const PagingList = ({ totalCount, unitSize, pageNum, setPageNum, pgPgNum, pgPgSi
       <Fragment key={i}>
         {
           // 페이징 번호와 현재 페이지 번호 일치시 b요소
-          i+1 === pageNum -1 ? (
+          i+1 === pageNum  ? (
             <b>{i+1}</b>
           ) : (
             <a
