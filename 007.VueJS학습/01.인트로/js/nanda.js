1603b0bf036ec5c55a476b3060c9b0f3262588ea
doyeun93 <doyeun93@naver.com>
2024-08-06 11:11:36 +0900

    s

--- 007.VueJS학습/01.인트로/js/nanda.js
+++ 007.VueJS학습/01.인트로/js/nanda.js
@@ -1,5 +1,32 @@
 // 스타일 난다 사이트 구성 js ///
 
+
+/****************************************************** 
+    [뷰 JS 라이트 사이클 속성 사용하기]
+    -> 뷰 인스턴스 생성자 메서드 객체 세팅시 사용
+        new Vue({ created(){} , mounted(){}, })
+
+    1. 뷰 인스턴스 초기화 완료 단계 : created
+    -> 이 단계 활용법 : 데이터 세팅을 많이 한다 
+    =>> 사용법 
+    (1) created : function(){코드}
+    (2) created :  ()=>{코드}
+    (3) created(){코드}
+
+    2. 뷰 랜더링 완료 단계(화면에 펼쳐지는) : mounted
+    -> 이 단계 활용법 : JS, 제이쿼리 등 DOM에 그려진 후
+                      코딩해야하는 부분을 여기에 연결한다
+    =>> 사용법  
+    (1) mounted : function(){코드}
+    (2) mounted : ()=>{코드}
+    (3) mounted(){코드}
+
+
+******************************************************/
+
+
+
+
 // 1. 뷰js 인스턴스 생성하기
 const vm = new Vue({
     // 1. 대상선정
@@ -12,6 +39,58 @@ const vm = new Vue({
         bigTit : "Style NANDA",
         // 2-2. 로고 태그 정보
         logo : `<img src="./images/logo_3ce.png" alt="nanda logo">`,
+        // 2-3. 배너 데이터
+        content:`
+            나는 날고 싶어~!
+            <h2>오늘도 당신은 날고 싶다~!</h2>
+            <img src="./images/sub_banner_e.jpg" alt="banner"> 
+        `,
+        // 2-4. 상품정보 배열
+        // 데이터를 클래스를 통해 생성하자
+        // ((예시데이터 셋팅))
+        // itemData:[
+        //     {
+        //         idx:1,
+        //         name:"vans와우",
+        //         img:"vans_1.jpg"
+        //     },
+        //     {
+        //         idx:2,
+        //         name:"vans올레이",
+        //         img:"vans_2.jpg"
+        //     },
+        //     {
+        //         idx:3,
+        //         name:"vans코코넛",
+        //         img:"vans_3.jpg"
+        //     },
+        //     {
+        //         idx:4,
+        //         name:"vans마크로",
+        //         img:"vans_4.jpg"
+        //     },
+        // ],
+
+        itemData: [],
+        // -> 배열 리터럴로 선언과 할당
     },
 
 }); ////// vue ////
+
+// 상품 정보 생성을 위한 생성자 클래스 ///////
+// 클래스는 객체 템플릿이다. 따라서 클래스 생성자 함수를 호출하면
+// 객체 인스턴스(메모리상에 실제 객체가 생성됨을 의미)가 생성된다
+class GetList {
+    // 생성자 메서드
+    constructor(idx,name,img, price){
+        // idx : 일련번호 
+        // name : 상품명
+        // img : 이미지 이름
+        // price : 상품 가격
+        this.idx = idx;
+        this.name = name;
+        this.img = img;
+        this.price = price;
+        // 여기서 this는 클래스 내부를 의미, GetList 인스턴스 자신을 의미한다
+    }
+} ///// getList 클래스 /////////
